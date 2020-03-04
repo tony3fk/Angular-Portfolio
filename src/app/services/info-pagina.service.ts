e6b4b32073c3d64d7f4523f41da21cca7bc55f1c
tony3fk <tony3fk@yahoo.es>
2020-03-04 21:20:48 +0100

    elementos desde bbdd firebase ok

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -1,30 +1,52 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { InfoPagina } from '../interfaces/info-pagina.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InfoPaginaService {
 
-  info: any = {};
+  info: InfoPagina = {};
   cargada: boolean = false;
+
+  equipo: any[] = [];
 
   constructor(private http: HttpClient) {
 
+
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
+
+
+  private cargarInfo() {
     // console.log('Servicio de infoPagina listo');
 
     //Leer el archivo json
     this.http.get('assets/data/data-pagina.json')
-      .subscribe(resp => {
+      .subscribe((resp: InfoPagina) => {
 
         this.cargada = true;
         this.info = resp;
-        console.log(resp);
+
 
 
       }
       );
 
+  }
 
+  private cargarEquipo() {
+    //Leer el archivo json
+    this.http.get('https://angular-html-f6743.firebaseio.com/equipo.json')
+      .subscribe((resp: any) => {
+
+        this.equipo = resp;
+        // console.log(resp);
+
+
+      }
+      );
   }
 }
