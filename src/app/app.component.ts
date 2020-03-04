f8d305a0d4f38d14d71a870c68915055d0aa538c
tony3fk <tony3fk@yahoo.es>
2020-03-04 22:48:30 +0100

    elemento espere por favor activado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { InfoPaginaService } from './services/info-pagina.service';
+import { ProductosService } from './services/productos.service';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +9,9 @@ import { InfoPaginaService } from './services/info-pagina.service';
 })
 export class AppComponent {
 
-  constructor(public InfoPaginaService: InfoPaginaService) {
+  constructor(public InfoPaginaService: InfoPaginaService, public productosService: ProductosService) {
+
+
 
 
   }
--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Producto } from '../interfaces/producto.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductosService {
 
-  constructor() { }
+  cargando = true;
+  productos: Producto[] = [];
+
+  constructor(private http: HttpClient) {
+    this.cargarProductos();
+  }
+
+
+  private cargarProductos() {
+
+    this.http.get('https://angular-html-f6743.firebaseio.com/productos_idx.json').subscribe((resp: Producto[]) => {
+
+      console.log(resp);
+      this.productos = resp;
+      this.cargando = false;
+
+
+    });
+
+  }
 }
